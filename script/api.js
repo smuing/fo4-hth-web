836279657d614500e54b543cd2dca3d02116efd8
NSM0924 <nasungmin50@naver.com>
2022-06-22 22:47:57 +0900

    api url, logo hide

--- script/api.js
+++ script/api.js
@@ -1,5 +1,5 @@
-// const API_URL = "https://fo4-hth-api.herokuapp.com";
-const API_URL = "http://localhost:3000";
+const API_URL = "https://fo4-hth-api.herokuapp.com";
+// const API_TEST_URL = "http://localhost:3000";
 
 const searchBtn = document.getElementById("search-btn");
 const cancelBtn = document.getElementById("cancel-btn");
